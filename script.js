79437102afceebb26da2c4dbc96c663aa5adfa7e
spiano <lafleurpiano@gmail.com>
2019-09-16 10:27:45 -0500

    added alerts to check for button connections

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 console.log('welcome to simon');
 
+let pickWedge = document.querySelector('section');
+pickWedge.addEventListener('click', function(evt) {
+    if (evt.target.className === 'wedge-choice1') {
+        alert('green!');
+    } else if (evt.target.className === 'wedge-choice2') {
+        alert('red!');
+    } else if (evt.target.className === 'wedge-choice3') {
+        alert('yellow!');
+    } else if (evt.target.className === 'wedge-choice4') {
+        alert('blue!');
+    } else if (evt.target.className === 'simon') {
+        alert('SIMON!');
+    }
+});
