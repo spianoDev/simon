2d212247afcc6956fe015aa5824f1bc18c202556
spiano <lafleurpiano@gmail.com>
2019-09-16 13:35:49 -0500

    changed buttons so they light up as they make a sound

--- script.js
+++ script.js
@@ -9,20 +9,34 @@ let pickSoundBlue = new Audio('sounds/low-c-(blue).mp3');
 let correctSound = new Audio('sounds/gliss-(correct).mp3');
 let buzzSound = new Audio('sounds/buzz-(incorrect).mp3');
 
-pickWedge.addEventListener('click', function(evt) {
+pickWedge.addEventListener('click', function (evt) {
     if (evt.target.className === 'wedge-choice1') {
-            pickSoundGreen.play();
-        }
-     else if (evt.target.className === 'wedge-choice2') {
+        document.querySelector('.wedge-choice1').style.backgroundColor = '#30bb0b';
+        pickSoundGreen.play();
+        setTimeout(() => {
+            document.querySelector('.wedge-choice1').style.backgroundColor = '#008000';
+        }, 800);
+    } else if (evt.target.className === 'wedge-choice2') {
+        document.querySelector('.wedge-choice2').style.backgroundColor = '#FF0000';
         pickSoundRed.play();
+        setTimeout(() => {
+            document.querySelector('.wedge-choice2').style.backgroundColor = '#B22222';
+        }, 800);
     } else if (evt.target.className === 'wedge-choice3') {
+        document.querySelector('.wedge-choice3').style.backgroundColor = '#FFFF00';
         pickSoundYellow.play();
+        setTimeout(() => {
+            document.querySelector('.wedge-choice3').style.backgroundColor = '#ffc303';
+        }, 800);
     } else if (evt.target.className === 'wedge-choice4') {
+        document.querySelector('.wedge-choice4').style.backgroundColor = '#0000FF';
         pickSoundBlue.play();
+        setTimeout(() => {
+            document.querySelector('.wedge-choice4').style.backgroundColor = '#00008B';
+        }, 800);
     } else if (evt.target.className === 'simon') {
-       // place hold the gliss sound and buzz sound
+        // place hold the gliss sound and buzz sound
         correctSound.play();
         buzzSound.play();
-
     }
 });
